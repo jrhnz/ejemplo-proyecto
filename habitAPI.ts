280eb8e87146084fd9ac0a7dd9da70fcc1861650
jrhnz <24001819@galileo.edu>
2025-03-05 23:56:27 -0600

    semana2

--- habitAPI.ts
+++
@@ -1,7 +0,0 @@
-export const fetchHabits = async () => {
-  const response = await fetch("http://localhost:3000/habitos");
-if (!response.ok) {
-  throw new Error("Failed to fetch habits");
-}
-  return await response.json();
-};
